222f8c72fae6c818bebab5641fdd8d317959252d
JING QIAN <jqian1590@gmail.com>
2015-11-10 21:21:16 -0500

    update

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var app = express();
 var hbs = require('hbs');
 var fs = require('fs');
 var path = require("path");
-var blogEngine = require('./blog');
-
+var fl = require('./fileLoader');
+var f = "";
 var p = "./data/";
+var fn = "haha";
 fs.readdir(p,function (err, files){
 		if(err){
 			console.log("unable to find");
@@ -18,23 +19,32 @@ fs.readdir(p,function (err, files){
 	        return fs.statSync(file).isFile();
 	    }).forEach(function (file) {
 	        console.log("%s (%s)", file, path.extname(file));
+	        f = file;
 	    });
 }) ;
+
+
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 var bodyParser = require('body-parser'); 
 app.use(bodyParser()); 
  
 app.get('/', function(req, res) {
-    res.render('index',{title:"My Blog", entries:blogEngine.getBlogEntries()});
+    res.render('index',{title:"My Blog", entries:fl.getBlogEntries(), list:f,fname:fn});
 });
  
 app.get('/about', function(req, res) {
     res.render('about', {title:"About Me"});
 });
+
+app.get('/graph/:fname', function(req, res) {
+    var fname = fl.getBlogEntry(req.params.fname);
+    res.render('graph');
+});
+
  
 app.get('/article/:id', function(req, res) {
-    var entry = blogEngine.getBlogEntry(req.params.id);
+    var entry = fl.getBlogEntry(req.params.id);
     res.render('article',{title:entry.title, blog:entry});
 });
  
